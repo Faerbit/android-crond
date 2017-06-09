539b016f544730409820ea746a2d13d31039d41f
Faerbit <faerbit@gmail.com>
2017-06-09 10:59:04 +0200

    Add wakeLock to execution.

--- app/src/main/java/it/faerb/crond/AlarmReceiver.java
+++ app/src/main/java/it/faerb/crond/AlarmReceiver.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.PowerManager;
 
 import static it.faerb.crond.Constants.INTENT_EXTRA_LINE_NAME;
 import static it.faerb.crond.Constants.INTENT_EXTRA_LINE_NO_NAME;
@@ -12,8 +13,13 @@ public class AlarmReceiver extends BroadcastReceiver {
 
     private static final String TAG = "AlarmReceiver";
 
+    private PowerManager.WakeLock wakeLock = null;
+
     @Override
     public void onReceive(Context context, Intent intent) {
+        PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        wakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG);
+        wakeLock.acquire();
         new LineExecuter(context).execute(intent);
     }
 
@@ -37,6 +43,7 @@ public class AlarmReceiver extends BroadcastReceiver {
             int lineNo = intent[0].getExtras().getInt(INTENT_EXTRA_LINE_NO_NAME);
             crond.executeLine(line, lineNo);
             crond.scheduleLine(line, lineNo);
+            wakeLock.release();
             return null;
         }
     }
